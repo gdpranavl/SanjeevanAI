781ae356b44bf92c152e82b68d4bd297193ce8c5
RaghottamNadgoudar <raghottam.nadgoudar2006p@gmail.com>
2025-06-07 01:20:03 +0530

    Made website to fetch data from website

--- frontend/src/app/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-50">
-      <h1 className="text-5xl font-bold text-primary">Hello, World! 👋</h1>
-    </div>
-  );
-}
